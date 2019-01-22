a4d68fc1bbbc9640aaec1b4905678b403af1bbb8
Jonas Stattin <jonas.i.stattin@gmail.com>
2019-01-22 18:57:31 +0100

    Added mocking example

--- kodexempel/testing/reactcomponents/src/components/DropDown.test.js
+++ kodexempel/testing/reactcomponents/src/components/DropDown.test.js
@@ -1,8 +1,8 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import React from 'react';
 import DropDown from './DropDown';
 
-test('Rendering an input box', () => {
+test('Rendering an dropdown box', () => {
   const dropDown = shallow(<DropDown items={['Diska', 'Städa', 'Sova']} />);
 
   expect(dropDown.find('li')).toHaveLength(0);
@@ -10,4 +10,32 @@ test('Rendering an input box', () => {
   dropDown.find('button').simulate('click');
 
   expect(dropDown.find('li')).toHaveLength(3);
+});
+
+test('force the toggle function', () => {
+  const dropDown = mount(<DropDown items={['Diska', 'Städa', 'Sova']} />);
+  
+  expect(dropDown.find('li')).toHaveLength(0);
+  
+  const instance = dropDown.instance();
+  instance.onToggleExpand();
+
+  dropDown.update();
+
+  expect(dropDown.find('li')).toHaveLength(3);
+});
+
+test('mock the toggle function', () => {
+  const dropDown = shallow(<DropDown items={['Diska', 'Städa', 'Sova']} />);
+  expect(dropDown.find('li')).toHaveLength(0);
+
+  dropDown.instance().onToggleExpand = jest.fn();
+
+  dropDown.instance().forceUpdate();
+  dropDown.update();
+
+  dropDown.find('button').simulate('click');
+
+  expect(dropDown.instance().onToggleExpand).toBeCalled();
+  expect(dropDown.find('li')).toHaveLength(0);
 });
